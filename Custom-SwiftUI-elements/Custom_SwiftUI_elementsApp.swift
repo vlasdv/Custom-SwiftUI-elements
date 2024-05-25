b3ba0d3c50425e5e16d11645856d2a99d8299e14
Dmitrii Vlasov <vlasdev@icloud.com>
2024-05-25 08:03:59 +0200

    Initial commit

---
+++ Custom-SwiftUI-elements/Custom_SwiftUI_elementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_SwiftUI_elementsApp.swift
+//  Custom-SwiftUI-elements
+//
+//  Created by Dmitrii Vlasov on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_SwiftUI_elementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
